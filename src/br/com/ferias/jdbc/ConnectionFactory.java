142b1e290ca94142ced373ff392ad95b45891b05
buracha <jadsoncanuto6@gmail.com>
2021-07-28 14:33:58 -0300

    ConnectionFactory criado

---
+++ src/br/com/ferias/jdbc/ConnectionFactory.java
@@ -0,0 +1,18 @@
+
+package br.com.ferias.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/BDVENDAS",
+                    "ferias","123");
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        
+    }
+}
---
+++ src/br/com/ferias/jdbc/testaConeccao.java
@@ -0,0 +1,16 @@
+
+package br.com.ferias.jdbc;
+
+import javax.swing.JOptionPane;
+
+
+public class testaConeccao {
+    public static void main(String[] args) {
+        try {
+            new ConnectionFactory().getConnection();
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar: /n"+e);
+        }
+    }
+}
