fbf48a10960dcedb2cd3e5be1f363775d8a81595
buracha <jadsoncanuto6@gmail.com>
2021-08-01 21:10:16 -0300

    Tela Funcionários pronto

--- src/br/com/ferias/view/frmFuncionarios.java
+++ src/br/com/ferias/view/frmFuncionarios.java
@@ -16,12 +16,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Jadson
  */
-public class frmCliente extends javax.swing.JFrame {
+public class frmFuncionarios extends javax.swing.JFrame {
 
     /**
      * Creates new form frmCliente
      */
-    public frmCliente() {
+    public frmFuncionarios() {
         initComponents();
     }
     
@@ -753,20 +753,21 @@ public class frmCliente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmFuncionarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmFuncionarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmFuncionarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmFuncionarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmCliente().setVisible(true);
+                new frmFuncionarios().setVisible(true);
             }
         });
     }
